fix: improve SVD gradient stability by clipping and masking homogenous component

common/models/CamLSTM.py
@@ -41,7 +41,7 @@ class CamLSTMBase(nn.Module):
         win_cam = inputs_cam.unfold(1, window_size, 1).squeeze().permute(axis_permutation)
 
         out = self(win_2d, win_cam)
-        return out.unsqueeze(1)
+        return out.view(1, n_windows, self.num_joints_out, self.out_features)
 
 
 class CoupledLSTM(CamLSTMBase):
@@ -65,7 +65,7 @@ class CoupledLSTM(CamLSTMBase):
         head_layers -- layer sizes for MLP head
         dropout -- dropout probability
         """
-        super().__init__(self, num_joints_in, in_features, num_joints_out, out_features,
+        super().__init__(num_joints_in, in_features, num_joints_out, out_features,
                  hidden_size, num_cells, head_layers, dropout)
 
         self.lstm_layers = nn.LSTM(num_joints_in*in_features + self.cam_mat_shape[0]*self.cam_mat_shape[1], 
@@ -100,12 +100,12 @@ class CoupledLSTM(CamLSTMBase):
         x = torch.cat([flattened_input_2d, flattened_input_cam], dim=2)
 
         # LSTM initial states
-        c_0 = torch.zeros(self.num_cells, x.shape[0], self.hidden_size)
         h_0 = torch.zeros(self.num_cells, x.shape[0], self.hidden_size)
+        c_0 = torch.zeros(self.num_cells, x.shape[0], self.hidden_size)
 
         if torch.cuda.is_available():
-            c_0 = c_0.cuda()
             h_0 = h_0.cuda()
+            c_0 = c_0.cuda()
 
         lstm_out, _ = self.lstm_layers(x, (h_0, c_0))
         mlp_out = self.mlp_layers(lstm_out[:, -1, :])
@@ -113,27 +113,25 @@ class CoupledLSTM(CamLSTMBase):
         return mlp_out.reshape(input_2d.shape[0], 1, self.num_joints_out, self.out_features)
 
 
-class UncoupledLSTM(nn.Module):
+class UncoupledLSTM(CamLSTMBase):
     cam_mat_shape = (3, 4)
 
-    def __init__(self, cam_intrinsics, num_joints_in, in_features, out_features, num_joints_out,
+    def __init__(self, num_joints_in, in_features, out_features, num_joints_out,
                  hidden_size, num_cells, head_layers, dropout=0.25):
         """ 
         Initialize this model.
 
         Arguments:
-        cam_intrinsics -- camera intrinsic matrix
         num_joints_in -- number of input joints (e.g. 17 for Human3.6M)
         in_features -- number of input features for each joint (typically 2 for 2D input)
         num_joints_out -- number of output joints (can be different than input)
+        out_features -- number of output features for each joint (typically 3 for 3D output)
         num_layers -- number of LSTM cells to stack
         hidden_size -- number of features to use in LSTM state
         head_layers -- layer sizes for MLP head
         dropout -- dropout probability
         """
-        self.cam_intrinsics = cam_intrinsics
-
-        super().__init__(self, num_joints_in, in_features, num_joints_out, out_features,
+        super().__init__(num_joints_in, in_features, num_joints_out, out_features,
             hidden_size, num_cells, head_layers)
 
         self.lstm_layers = nn.LSTM(num_joints_in*in_features + self.cam_mat_shape[0]*self.cam_mat_shape[1], 
@@ -161,9 +159,14 @@ class UncoupledLSTM(nn.Module):
         flat_cam  = input_cam.view(B, T, H * W)
         x = torch.cat([flat_pose, flat_cam], dim=-1)
 
-        h0 = x.new_zeros(self.num_cells, B, self.hidden_size)
-        c0 = x.new_zeros_like(h0)
-        lstm_out, _ = self.lstm_layers(x, (h0, c0))
+        h_0 = torch.zeros(self.num_cells, B, self.hidden_size)
+        c_0 = torch.zeros(self.num_cells, B, self.hidden_size)
+
+        if torch.cuda.is_available():
+            h_0 = h_0.cuda()
+            c_0 = c_0.cuda()
+
+        lstm_out, _ = self.lstm_layers(x, (h_0, c_0))
 
         lambdas = torch.sigmoid(self.mlp_layers(lstm_out))
         lambda_pose, lambda_cam = lambdas[..., 0], lambdas[..., 1]
@@ -175,7 +178,8 @@ class UncoupledLSTM(nn.Module):
         ref_cam  = ref_cam.view(B, self.cam_mat_shape[0], self.cam_mat_shape[1])
 
         # Triangulate points
-        triangulated_poses = self.triangulate_points_batched(ref_cam, input_cam[:, T//2, :, :], ref_pose, input_2d[:, T//2, :, :])
+        triangulated_poses = self.triangulate_points_batched(input_cam[:, 0, :, :], input_cam[:, T//2, :, :],
+                                                             input_2d[:, 0, :, :], input_2d[:, T//2, :, :])
         return triangulated_poses.reshape(B, 1, self.num_joints_out, self.out_features)
 
     def compute_filtered(self, ref_seq, lambda_seq):
@@ -183,7 +187,8 @@ class UncoupledLSTM(nn.Module):
         cumprod_rev = torch.cumprod(rev, dim=1)
         cumprod = torch.flip(cumprod_rev, dims=[1])
 
-        prod_next = torch.cat([cumprod[:, 1:], torch.ones(lambda_seq.shape[0], 1)], dim=1)
+        prod_next = torch.cat([cumprod[:, 1:],
+                               torch.ones(lambda_seq.shape[0], 1, device=lambda_seq.device)], dim=1)
         if torch.cuda.is_available():
             prod_next = prod_next.cuda()
         w = (1.0 - lambda_seq) * prod_next
@@ -192,11 +197,8 @@ class UncoupledLSTM(nn.Module):
             w = w.unsqueeze(-1)
         return (ref_seq * w).sum(dim=1)
 
-    def triangulate_points_batched(self, extrinsic_a, extrinsic_b, screen_a, screen_b):
+    def triangulate_points_batched(self, cam_mat_a, cam_mat_b, screen_a, screen_b):
         B, N = screen_a.shape[0], screen_a.shape[1]
-
-        cam_mat_a = self.cam_intrinsics @ extrinsic_a
-        cam_mat_b = self.cam_intrinsics @ extrinsic_b
 
         hom_screen_a = torch.cat([screen_a, torch.ones(B, N, 1, device=screen_a.device, dtype=screen_a.dtype)], dim=-1)
         hom_screen_b = torch.cat([screen_b, torch.ones(B, N, 1, device=screen_b.device, dtype=cam_mat_b.dtype)], dim=-1)
@@ -219,11 +221,20 @@ class UncoupledLSTM(nn.Module):
         A[:, 2, :] = hom_screen_b[:, 0:1] * cam_mat_b[:, 2, :] - cam_mat_b[:, 0, :]
         A[:, 3, :] = hom_screen_b[:, 1:2] * cam_mat_b[:, 2, :] - cam_mat_b[:, 1, :]
 
-        # Solve with SVD
-        _, _, Vh = torch.linalg.svd(A)
-        hom_triangulated_pose = Vh[:, -1, :]  # (B*N, 4)
+        # 4. solve
+        _, _, Vh = torch.linalg.svd(A, full_matrices=False)
+        hom = Vh[:, -1]                                   # (B*N, 4)
 
-        # Perspective Projection
-        triangulated_pose = hom_triangulated_pose[:, :self.out_features] / hom_triangulated_pose[:, self.out_features:self.out_features+1]
+        # 5. normalise robustly
+        scale = hom.abs().amax(dim=1, keepdim=True)
+        hom = hom / scale                                 # now max component ≈1
+        w = hom[:, 3:4]
 
-        return triangulated_pose.reshape(B, N, self.out_features)
+        # optional masking
+        bad = w.abs() < 0.005
+        w_safe = torch.where(bad, torch.ones_like(w), w)
+
+        xyz = hom[:, :3] / w_safe
+        xyz[bad.expand_as(xyz)] = float('nan')            # mark invalid
+
+        return xyz.reshape(B, N, 3)